bf3c8bd73a94663813582c13f8935cce3b89484d
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2022-10-22 17:21:49 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const { response } = require('express');
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+
+const categories = require('./data/categories.json')
+
+app.get('/' , (req,res) => {
+    res.send('love you too!');
+})
+
+app.get('/news-categories', (req,res)=>{
+    res.send(categories);
+})
+
+app.listen(port , ()=>{
+    console.log("Dragon news server runnint on port", port);
+})
